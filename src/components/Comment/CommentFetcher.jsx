3414e163a1a556cc132fe89ace304d8a0529364d
Bryan Garcia <bgarci55@depaul.edu>
2025-06-18 22:29:48 -0500

    Added Comment Generation

--- src/components/Comment/CommentFetcher.jsx
+++ src/components/Comment/CommentFetcher.jsx
@@ -1,24 +1,58 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import CommentHandler from '../Comment/CommentFormat';
 
 function CommentFetcher() {
-    const [post, setPost] = useState(null);
+    const [posts, setPosts] = useState([]);
+    const [visiblePosts, setVisiblePosts] = useState(2);
+    const loaderRef = useRef(null);
 
     useEffect(() => {
-        const randomId = Math.floor(Math.random() * 150) + 1;
-        const url = `https://dummyjson.com/posts/${randomId}`;
-
+        const url = `https://dummyjson.com/posts/`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                setPost(data);
-                console.log("API Response:", data);
+                setPosts(data.posts || []);
+                setVisiblePosts(2);
+                console.log("API Response:", data.posts);
             });
     }, []);
 
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+        ([entry]) => {
+            if (entry.isIntersecting) {
+            setVisiblePosts((prev) => {
+                const next = prev + 2;
+                return posts ? Math.min(next, posts.length) : prev;
+            });
+            }
+        },
+        {
+            root: null,
+            rootMargin: '100px',
+            threshold: 1.0,
+        }
+    );
+
+    if (loaderRef.current) observer.observe(loaderRef.current);
+    
+    return () => {
+        if (loaderRef.current) observer.disconnect();
+        };
+    }, [posts]);
+
     return (
     <div>
-        {post ? <CommentHandler post={post} /> : <p>Loading...</p> }
+        {posts.length > 0 ? (
+        <>
+        {posts.slice(0, visiblePosts).map((post) => (
+        <CommentHandler key = {post.id} post={post} />
+        ))}
+            <div ref={loaderRef} />
+        </>
+        ) : (
+            <p>Loading...</p>
+        )}
     </div>
     );
 }
